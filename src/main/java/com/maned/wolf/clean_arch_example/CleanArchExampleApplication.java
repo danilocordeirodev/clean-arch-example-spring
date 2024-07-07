4ead95e804c5a1a6dd57fe9f7c81d7084e0fac31
Danilo Cordeiro <danilocordeiro.ti@gmail.com>
2024-07-07 18:28:52 -0300

    initial project

---
+++ src/main/java/com/maned/wolf/clean_arch_example/CleanArchExampleApplication.java
@@ -0,0 +1,13 @@
+package com.maned.wolf.clean_arch_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanArchExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanArchExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maned/wolf/clean_arch_example/CleanArchExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maned.wolf.clean_arch_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanArchExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
